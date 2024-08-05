e44444c3c5b0dbc66ba2cf70fc15fd449e563d7c
DeMonGl00mNew <108478342+DeMonGl00mNew@users.noreply.github.com>
2024-08-05 11:43:59 +0400

    Add files via upload
    change send.cs

--- Assets/Send.cs
+++ Assets/Send.cs
@@ -7,7 +7,7 @@ using System.Net.Security;
 using System.Security.Cryptography.X509Certificates;
 using TMPro;
 
-//СЃРєСЂРёРїС‚, РѕС‚РІРµС‡Р°СЋС‰РёР№ Р·Р° Р»РѕРіРёРєСѓ СЂР°СЃСЃС‹Р»РєРё РїРёСЃРµРј РєР»РёРµРЅС‚Р°Рј.
+//скрипт, отвечающий за логику рассылки писем клиентам.
 
 public class Send : MonoBehaviour
 {
@@ -19,27 +19,27 @@ public class Send : MonoBehaviour
      public string emailSvoy= "oktyabskmerch@gmail.com";
     public string passwordSvoy = "ksrbaytkO";
     private bool couratinIsActive = false;
-    //С„СѓРЅРєС†РёСЏ РЅР°СЃС‚СЂРѕР№РєРё Рё РѕС‚РїСЂР°РІРєРё РїРёСЃСЊРјР° Р°РґРјРёРЅРёСЃС‚СЂР°С‚РѕСЂСѓ Рё РєР»РёРµРЅС‚Р°Рј
+    //функция настройки и отправки письма администратору и клиентам
     public void send()
    {
         
         emailKomu = AuthUser.Instance.CurrentEmail;
         if(!couratinIsActive) StartCoroutine(AlertBuyAndPost());
-        SetupMessageAndSend("Р§РµРє Рѕ РїРѕРєСѓРїРєРµ",
-            $"Р—Р°РєР°Р· РїСЂРёРЅСЏС‚. РќР°РёРјРµРЅРѕРІР°РЅРёРµ С‚РѕРІР°СЂР°: {NazvnieTowara.text}; СЂР°Р·РјРµСЂ: {RazmerTovara.text}; С†РµРЅР°: {Cena.text}.",
+        SetupMessageAndSend("Чек о покупке",
+            $"Заказ принят. Наименование товара: {NazvnieTowara.text}; размер: {RazmerTovara.text}; цена: {Cena.text}.",
              emailSvoy,
              passwordSvoy,
              emailKomu
             );
         
-        SetupMessageAndSend("Р§РµРє Рѕ Р·Р°РєР°Р·Рµ",
-    $"РєР»РёРµРЅС‚ {emailKomu}; РЅР°РёРјРµРЅРѕРІР°РЅРёРµ С‚РѕРІР°СЂР°: {NazvnieTowara.text}; СЂР°Р·РјРµСЂ: {RazmerTovara.text}; С†РµРЅР°: {Cena.text}.",
+        SetupMessageAndSend("Чек о заказе",
+    $"клиент {emailKomu}; наименование товара: {NazvnieTowara.text}; размер: {RazmerTovara.text}; цена: {Cena.text}.",
             emailSvoy,
             passwordSvoy,
             emailSvoy
     );
     }
-    //С„СѓРЅРєС†РёСЏ РѕС‚РїСЂР°РІРєРё РїРёСЃСЊРјР°
+    //функция отправки письма
     private void SetupMessageAndSend (string tema, string soderganie,string otkuda, string passwordOtpravitely,string kuda)
     {
         MailMessage message = new MailMessage();
@@ -60,16 +60,15 @@ public class Send : MonoBehaviour
         client.Send(message);
     }
 
-    //СЃРѕРїСЂРѕРіСЂР°РјРјР° РґР»СЏ РѕРїРѕРІРµС‰РµРЅРёСЏ РѕР± РѕС‚РїСЂР°РІРєРё РїРёСЃСЊРјР° РїРѕР»СЊР·РѕРІР°С‚РµР»СЋ
+    //сопрограмма для оповещения об отправки письма пользователю
     IEnumerator AlertBuyAndPost()
     {
         couratinIsActive = true;
         Alert.gameObject.SetActive(true);
-        Alert.text =$"РўРѕРІР°СЂ РєСѓРїР»РµРЅ, РїСЂРѕРІРµСЂСЊС‚Рµ РїРѕС‡С‚Сѓ {emailKomu} ";
+        Alert.text =$"Товар куплен, проверьте почту {emailKomu} ";
         yield return new WaitForSeconds(4f);
         Alert.gameObject.SetActive(false);
         couratinIsActive = false;
     }
 
 }
-
